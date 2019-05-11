063ae14c208e63cf044bcfc3027d4211b2796ada
edwardreeve <edreeve+git@gmail.com>
2019-05-11 11:50:18 +0100

    Added a student count, and comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# First, we print a list of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,7 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# Then, we print the number of current students
+print "Overall, we have "
+print 11
+print " great students"
