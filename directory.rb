f7e746fd74fcb9f373cf7da7b1046d35bdaa09cf
edwardreeve <edreeve+git@gmail.com>
2019-05-11 11:53:01 +0100

    Refactored student count using interpolated variable

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 11
 # First, we print a list of students
 puts "The students of Villains Academy"
 puts "-------------"
@@ -13,6 +14,4 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 # Then, we print the number of current students
-print "Overall, we have "
-print 11
-print " great students"
+puts "Overall, we have #{student_count} great students"
