1f95b3ee6acb943227176b4f29a337c447853d81
edwardreeve <edreeve+git@gmail.com>
2019-05-12 12:01:49 +0100

    Added the capture of hobbies and height

--- testing_directory.rb
+++ testing_directory.rb
@@ -2,19 +2,37 @@ def input_students
   puts 'Please enter the names of the students'
   puts 'To finish, just hit return twice'
   # create an empty array
-  students = []
+  @students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << { name: name, cohort: :november }
-    puts "Now we have #{students.count} students"
+    @students << { name: name, cohort: :november }
+    puts "Now we have #{@students.count} students"
     # get another name from the user
     name = gets.chomp
   end
   # return the array of students
-  students
+  @students
+end
+
+def input_hobbies
+  puts 'Now enter each student\'s hobbies'
+  @students.each do |student|
+    puts "#{student[:name]}'s hobbies;"
+    hobbies = gets.chomp
+    student[:hobbies] = hobbies
+  end
+end
+
+def input_height
+  puts 'Now enter each student\'s height in cm'
+  @students.each do |student|
+    puts "#{student[:name]}'s height;"
+    height = gets.chomp
+    student[:height] = height
+  end
 end
 
 def print_header
@@ -25,7 +43,7 @@ end
 def print(students)
   ind = 0
   until ind == students.length
-    puts "#{ind + 1}. #{students[ind][:name]}, #{students[ind][:cohort]} cohort"
+    puts "#{ind + 1}. #{students[ind][:name]}, enjoys #{students[ind][:hobbies]}, #{students[ind][:height]}cm, #{students[ind][:cohort]} cohort"
     ind += 1
   end
 end
@@ -34,6 +52,8 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 students = input_students
+input_hobbies
+input_height
 # nothing happens until we call each method
 print_header
 print(students)
